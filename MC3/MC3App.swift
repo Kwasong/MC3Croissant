58d5002325230873e2896343d3b49717be7f129c
Wahyu Alfandi <w.alfa433@gmail.com>
2023-07-15 12:30:59 +0700

    Initial Commit

---
+++ MC3/MC3App.swift
@@ -0,0 +1,17 @@
+//
+//  MC3App.swift
+//  MC3
+//
+//  Created by Wahyu Alfandi on 15/07/23.
+//
+
+import SwiftUI
+
+@main
+struct MC3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

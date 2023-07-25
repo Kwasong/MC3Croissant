bea3e2fa0631e80403ed05ff2914a73e758c46dd
Wahyu Alfandi <w.alfa433@gmail.com>
2023-07-25 12:09:12 +0700

    eleven labs api call

--- MC3/App/ContentView.swift
+++ MC3/App/ContentView.swift
@@ -6,15 +6,34 @@
 //
 
 import SwiftUI
+import AVFAudio
 
 struct ContentView: View {
     @AppStorage("isShowingOnboarding") var isShowingOnboarding: Bool = true
+    @State var audioPlayer: AVAudioPlayer?
+    
+    
     var body: some View {
         if isShowingOnboarding {
             AlbumListView()
                 .onAppear{
-                    guard let apiKey: String = Bundle.main.infoDictionary?["API_KEY"] as? String else {return}
-                    print(apiKey)
+                    ElevenLabsService.sharedInstance.fetchTextToSpeech(text: "the GPT family of models process text using tokens, which are common sequences of characters found in text. The models understand the statistical relationships between these tokens, and excel at producing the next token in a sequence of tokens.") { remoteResponse in
+                        switch remoteResponse {
+                        case .success(let data):
+                            print("disini masuk success")
+                            
+                            do{
+                                audioPlayer = try AVAudioPlayer(data: data)
+                                audioPlayer?.prepareToPlay()
+                                audioPlayer?.play()
+                            }catch {
+                                print("gagal play audio")
+                            }
+                            
+                        case .failure(let failure):
+                            print("failure")
+                        }
+                    }
                 }
         } else {
             VStack{
@@ -28,5 +47,6 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .environmentObject(MusicViewModel())
     }
 }

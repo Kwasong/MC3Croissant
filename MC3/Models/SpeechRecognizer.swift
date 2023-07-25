627d56edefd5168b44afe24702228ecdf6603bfd
Safik <safikwidiantoro@gmail.com>
2023-07-25 15:33:13 +0700

    add guard

--- MC3/Models/SpeechRecognizer.swift
+++ MC3/Models/SpeechRecognizer.swift
@@ -27,26 +27,25 @@ extension SpeechRecognizer {
     private func startRecognition() {
         recognizedText = ""
         silenceTimer?.invalidate()
-
+        
         DispatchQueue.global(qos: .userInitiated).async { [weak self] in
-            self?.recognitionTask?.cancel()
-            self?.recognitionTask = nil
+            guard let self = self else { return }
             
-            self?.request = SFSpeechAudioBufferRecognitionRequest()
+            self.recognitionTask?.cancel()
+            self.recognitionTask = nil
             
-            guard let request = self?.request else {
+            self.request = SFSpeechAudioBufferRecognitionRequest()
+            
+            guard let request = self.request else {
                 print("Unable to create request.")
                 return
             }
             
-            guard let inputNode = self?.audioEngine.inputNode else {
-                print("Audio engine has no input node.")
-                return
-            }
+            let inputNode = self.audioEngine.inputNode 
             
             request.shouldReportPartialResults = true
             
-            self?.recognitionTask = self?.speechRecognizer?.recognitionTask(with: request) { [weak self] result, error in
+            self.recognitionTask = self.speechRecognizer?.recognitionTask(with: request) { [weak self] result, error in
                 guard let result = result else {
                     print("Recognition failed: \(error?.localizedDescription ?? "No result")")
                     return
@@ -60,19 +59,19 @@ extension SpeechRecognizer {
             
             let recordingFormat = inputNode.outputFormat(forBus: 0)
             inputNode.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) { buffer, _ in
-                self?.request?.append(buffer)
+                self.request?.append(buffer)
             }
             
             
-            self?.audioEngine.prepare()
+            self.audioEngine.prepare()
             
             do {
-                try self?.audioEngine.start()
+                try self.audioEngine.start()
                 
                 DispatchQueue.main.async {
-                    self?.isRecognizing.toggle()
+                    self.isRecognizing.toggle()
                 }
-         
+                
             } catch {
                 print("Could not start audio engine: \(error)")
             }
@@ -84,9 +83,9 @@ extension SpeechRecognizer {
     private func startSilenceTimer() {
         silenceTimer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { [weak self] _ in
             guard let self = self else { return }
-
+            
             self.silenceDuration += 0.1
-
+            
             if self.silenceDuration >= 2.0 {
                 // Print "woi" when 2 seconds of silence have passed
                 //TODO: Prompt chat gpt
@@ -99,13 +98,14 @@ extension SpeechRecognizer {
     
     private func stopRecognition() {
         DispatchQueue.global(qos: .userInitiated).async { [weak self] in
-            self?.audioEngine.stop()
-            self?.audioEngine.inputNode.removeTap(onBus: 0)
-            self?.request = nil
-            self?.recognitionTask = nil
+            guard let self = self else { return }
+            self.audioEngine.stop()
+            self.audioEngine.inputNode.removeTap(onBus: 0)
+            self.request = nil
+            self.recognitionTask = nil
             
             DispatchQueue.main.async {
-                self?.isRecognizing.toggle()
+                self.isRecognizing.toggle()
             }
         }
         self.silenceTimer?.invalidate()
@@ -119,7 +119,7 @@ extension SpeechRecognizer {
                 return "\(word) "
             }
             let finalText = finalWords.joined(separator: " ")
-
+            
             DispatchQueue.main.async {
                 self?.recognizedText = finalText
             }

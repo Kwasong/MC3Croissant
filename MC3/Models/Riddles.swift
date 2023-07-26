9a2239a2c2e24ed27d42b97ab749211c72cfc97d
Sha Nia Siahaan <shaniashn76@gmail.com>
2023-07-26 12:51:09 +0700

    add more riddles

--- MC3/Models/Riddles.swift
+++ MC3/Models/Riddles.swift
@@ -14,9 +14,16 @@ struct Riddles {
 
 var riddle: [Riddles] = [
     Riddles(question: "What did the ocean say to the beach?", answer: "Nothing, it just waved"),
-    Riddles(question: "Where do fruits go on vacation?", answer: "Pear-is!"),
-    Riddles(question: "What comes once in a minute, twice in a moment, but never in a thousand years?", answer: "The letter \"M\""),
-    Riddles(question: "What has keys but can't open locks, has space but has no room, and you can enter, but you can't go outside?", answer: "Keyboard"),
-    Riddles(question: "What has keys but can't open locks?", answer: "A piano"),
-    Riddles(question: "I speak without a mouth and hear without ears. I have no body, but I come alive with the wind. What am I?", answer: "An echo")
+    Riddles(question: "What did Baby Corn say to Mama Corn", answer: "Where's Pop Corn?"),
+    Riddles(question: "What do you call someone with no body and no nose?", answer: "Nobody knows."),
+    Riddles(question: "What does a lemon say when it answers the phone?", answer: "Yellow!"),
+    Riddles(question: "How do you make 7 even?", answer: "Take away the s."),
+    Riddles(question: "How many tickles does it take to make an octopus laugh", answer: "Ten tickles."),
+    Riddles(question: "What kind of shoes do ninjas wear?", answer: "Sneakers!"),
+    Riddles(question: "What do you call an elephant that doesn't matter?", answer: "An irrelephant."),
+    Riddles(question: "What's the best-smelling insect?", answer: "A deodor-ant."),
+    Riddles(question: "What do you call a fake noodle?", answer: "An impasta."),
+    Riddles(question: "What happens when a strawberry gets run over crossing the street?", answer: "Traffic jam."),
+    Riddles(question: "What do clouds wear?", answer: "Thunderwear."),
+    Riddles(question: "Why is Peter Pan always flying?", answer: "He neverlands.")
 ]
